61c905fb17509572d69329a440ae60766e52205e
Floriane Andrieux <florianeandrieux@MacBook-Pro-de-Floriane.local>
2025-09-22 21:10:26 -0300

    Déploiement monitor.js corrigé avec express

--- monitor.js
+++ monitor.js
@@ -1,17 +1,17 @@
 const puppeteer = require('puppeteer');
 const fetch = require('node-fetch');
-const chalk = require('chalk'); // Pour des logs colorés et plus sexy
+const chalk = require('chalk'); 
+const express = require('express'); // 👈 Nouveau serveur
+const app = express();
 
-// ⚡ Votre token et chat ID Telegram
 const TELEGRAM_TOKEN = '8249846675:AAFc5uAjkhFWRzXTo73wvv8mmOYTRfh7CPE';
 const CHAT_ID = '8291065466';
 
-// URL et texte à surveiller
 const URL = 'https://www.quintoandar.com.br/alugar/imovel/rio-de-janeiro-rj-brasil/de-500-a-3500-reais?geoJson=%7B%22type%22%3A%22Feature%22%2C%22geometry%22%3A%7B%22type%22%3A%22Polygon%22%2C%22coordinates%22%3A%5B%5B%5B-43.23600955289077%2C-22.979703796453084%5D%2C%5B-43.23326297085952%2C-22.995190718919638%5D%2C%5B-43.182966187412255%2C-22.986341266381395%5D%2C%5B-43.18639941495132%2C-22.981600249709548%5D%2C%5B-43.19652743619155%2C-22.984128812641217%5D%2C%5B-43.20408053677749%2C-22.977175150704092%5D%2C%5B-43.22261996548843%2C-22.97591081008484%5D%2C%5B-43.23600955289077%2C-22.979703796453084%5D%5D%5D%7D%2C%22properties%22%3A%7B%7D%7D';
 const SELECTOR = '#__next > div.cozy__theme--default.cozy__theme--default-next > div > main > section.SideMenuHouseList_customSection__XJ5QW > div > div.EmptySearchState_wrapper__TKEeL > div > h3';
 const TEXT_TO_CHECK = 'Não há imóveis no QuintoAndar para esta busca.';
 
-let lastState = true; // true = pas d’annonce, false = annonce détectée
+let lastState = true;
 
 async function sendTelegram(msg) {
     try {
@@ -58,6 +58,11 @@ async function checkPage() {
     }
 }
 
-// Check toutes les 30 secondes
+// Lancer le monitoring toutes les 30 secondes
 console.log(chalk.cyan('💎 Monitoring QuintoAndar démarré toutes les 30 secondes...'));
 setInterval(checkPage, 30000);
+
+// 👇 Serveur Express pour EvenNode
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('💎 Monitoring QuintoAndar actif !'));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
